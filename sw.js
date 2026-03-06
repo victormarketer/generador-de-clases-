2c7abfa9d1eefc83f01628410f48cd96a9c466c1
victormarketer <113622180+victormarketer@users.noreply.github.com>
2026-03-06 16:24:07 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,22 @@
+const CACHE_NAME = "clases-celulas-v3";
+
 self.addEventListener("install", (event) => {
-  console.log("Service Worker instalado");
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
-  console.log("Service Worker activo");
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", function(event) {
