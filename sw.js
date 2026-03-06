3b6407b727ee851a9c5ba680e96d4d4f94df4fc2
victormarketer <113622180+victormarketer@users.noreply.github.com>
2026-03-06 11:33:38 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener("install", (event) => {
+  console.log("Service Worker instalado");
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker activo");
+});
+
+self.addEventListener("fetch", function(event) {
+});
